06b49b1c151448c4c87d299f0ef5b47021ce80df
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-04-14 13:47:31 +0100

    Change port back to be 3000 in the config

--- app/config.js
+++ app/config.js
@@ -8,7 +8,7 @@ module.exports = {
   serviceName: " ",
 
   // Default port that prototype runs on
-  port: '80',
+  port: '3000',
 
   // Enable or disable password protection on production
   useAuth: 'true'
