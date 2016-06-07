1d359e3ac8fbebc6853cb55fe89988338a405cd2
rob jenkins <rob@knowdigital.co.uk>
2016-06-07 14:36:38 +0100

    proto-type-admin-page

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -25,3 +25,21 @@ $( document ).ready(function() {
   });
 
 
+
+var CompanyName = function() {
+ var navStatus = JSON.parse(localStorage.getItem('company-name-header'));
+ console.log(navStatus);
+    if (navStatus == null) {
+      navStatus = "Acme Ltd Coventry";
+      localStorage.setItem("company-name-header", JSON.stringify(navStatus));
+  } else {
+  }
+};
+CompanyName();
+
+//This sets the value of the company field on the prototype admin page - shouldn't do anything else.
+
+$( document ).ready(function() {
+var bla = JSON.parse(localStorage.getItem('company-name-header'));
+$('#company-name-field').val(bla);
+});
