794f00d25e55d69aaa853015f7217fcd5637260f
Simon Bull <sbulluk@users.noreply.github.com>
2016-08-01 15:37:59 +0100

    Split commitment model and view for list and fix issues with double eval of scripts in content block

--- app/assets/javascripts/das/commitment-1.0.js
+++ app/assets/javascripts/das/commitment-1.0.js
@@ -1,79 +1,32 @@
 (function(){
-    var view = {
-        $noApprenticeshipsBlock: null,
-        $apprenticeshipsBlock: null,
-        $numberOfApprenticesLabel: null,
-        $apprenticeshipsTableBody: null,
-
-        load: function() {
-            view.$noApprenticeshipsBlock = $('#no-apprenticeships');
-            view.$apprenticeshipsBlock = $('#apprenticeships');
-            view.$numberOfApprenticesLabel = $('#numberOfApprentices');
-            view.$apprenticeshipsTableBody = $('#apprenticeships table > tbody');
-        }
-    };
     var model = {
         apprenticeships: [],
 
         load: function() {
             var json = window.localStorage.getItem('commitment-apprenticeships');
-            model.apprenticeships  = JSON.parse(json);
+            model.apprenticeships  = JSON.parse(json) || [];
         },
         save: function() {
             window.localStorage.setItem('commitment-apprenticeships', JSON.stringify(model.apprenticeships));
         }
     };
 
-    function showEmptyState() {
-        $('#no-apprenticeships').removeClass('rj-dont-display');
-        $('#apprenticeships').addClass('rj-dont-display');
-    }
-    function showApprenticeships() {
-        $('#no-apprenticeships').addClass('rj-dont-display');
-        $('#apprenticeships').removeClass('rj-dont-display');
-
-        view.$numberOfApprenticesLabel.text(model.apprenticeships.length);
-        for(var i = 0; i < model.apprenticeships.length; i++) {
-            var $tr = makeRow(model.apprenticeships[i]);
-            view.$apprenticeshipsTableBody.append($tr);
-        }
-    }
-    function makeRow(apprenticeship) {
-        var $tr = $('<tr></tr>');
-        $tr.append(makeCell(apprenticeship.training.name));
-        $tr.append(makeCell(apprenticeship.apprentice.first));
-        $tr.append(makeCell(apprenticeship.apprentice.last));
-        $tr.append(makeCell(apprenticeship.apprentice.uln));
-        $tr.append(makeCell(apprenticeship.start.month + '/' + apprenticeship.start.year));
-        $tr.append(makeCell(apprenticeship.finish.month + '/' + apprenticeship.finish.year));
-        $tr.append(makeCell('£' + apprenticeship.cost.format(0)));
-        $tr.append(makeCell(''));
-        return $tr;
-    }
-    function makeCell(text) {
-        return $('<td></td>').text(text);
-    }
-
-    $(document).ready(function() {
-        view.load();
-        model.load();
-
-        if(model.apprenticeships.length > 0) {
-            showApprenticeships();
-        }
-        else {
-            showEmptyState();
-        }
-    });
+    model.load();
 
     // Exports
     window.das = window.das || {};
     window.das.commitments = {
+        getAll: function() {
+          return model.apprenticeships;
+        },
         addApprenticeships: function(apprenticeships) {
             for(var i = 0; i < apprenticeships.length; i++) {
                 model.apprenticeships.push(apprenticeships[i]);
             }
             model.save();
+        },
+        update: function(apprenticeship) {
+
         },
         clear: function() {
             model.apprenticeships = [];
---
+++ public/javascripts/das/list-commitment-1.0.js
@@ -0,0 +1,59 @@
+(function() {
+    var view = {
+        $noApprenticeshipsBlock: null,
+        $apprenticeshipsBlock: null,
+        $numberOfApprenticesLabel: null,
+        $apprenticeshipsTableBody: null,
+
+        load: function() {
+            view.$noApprenticeshipsBlock = $('#no-apprenticeships');
+            view.$apprenticeshipsBlock = $('#apprenticeships');
+            view.$numberOfApprenticesLabel = $('#numberOfApprentices');
+            view.$apprenticeshipsTableBody = $('#apprenticeships table > tbody');
+        }
+    };
+
+    function showEmptyState() {
+        $('#no-apprenticeships').removeClass('rj-dont-display');
+        $('#apprenticeships').addClass('rj-dont-display');
+    }
+    function showApprenticeships(apprenticeships) {
+        $('#no-apprenticeships').addClass('rj-dont-display');
+        $('#apprenticeships').removeClass('rj-dont-display');
+
+        view.$numberOfApprenticesLabel.text(apprenticeships.length);
+        for(var i = 0; i < apprenticeships.length; i++) {
+            var $tr = makeRow(apprenticeships[i]);
+            view.$apprenticeshipsTableBody.append($tr);
+        }
+    }
+    function makeRow(apprenticeship) {
+        var $tr = $('<tr></tr>');
+        $tr.append(makeCell(apprenticeship.training.name));
+        $tr.append(makeCell(apprenticeship.apprentice.first));
+        $tr.append(makeCell(apprenticeship.apprentice.last));
+        $tr.append(makeCell(apprenticeship.apprentice.uln));
+        $tr.append(makeCell(apprenticeship.start.month + '/' + apprenticeship.start.year));
+        $tr.append(makeCell(apprenticeship.finish.month + '/' + apprenticeship.finish.year));
+        $tr.append(makeCell('£' + apprenticeship.cost.format(0)));
+        $tr.append(makeCell(''));
+        return $tr;
+    }
+    function makeCell(text) {
+        return $('<td></td>').text(text);
+    }
+
+    $(document).ready(function() {
+        console.log('load commitment list');
+        view.load();
+
+        var apprenticeships = window.das.commitments.getAll();
+        if(apprenticeships.length > 0) {
+            showApprenticeships(apprenticeships);
+        }
+        else {
+            showEmptyState();
+        }
+    });
+
+})();
