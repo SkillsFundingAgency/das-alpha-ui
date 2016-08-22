5110cdc45de524c2d27e606d2a53966d576429fb
rob jenkins <rob@knowdigital.co.uk>
2016-08-22 14:04:35 +0100

    updates

--- app/assets/javascripts/das/list-commitment-1.0.js
+++ app/assets/javascripts/das/list-commitment-1.0.js
@@ -1,3 +1,4 @@
+
 (function() {
     var view = {
         $noApprenticeshipsBlock: null,
@@ -10,6 +11,8 @@
             view.$apprenticeshipsBlock = $('#apprenticeships');
             view.$numberOfApprenticesLabel = $('#numberOfApprentices');
             view.$apprenticeshipsTableBody = $('#apprenticeships table > tbody');
+
+
         }
     };
 
@@ -21,47 +24,39 @@
         $('#no-apprenticeships').addClass('rj-dont-display');
         $('#apprenticeships').removeClass('rj-dont-display');
 
+
+        // random global variable assignment
+        numberOfApps = apprenticeships.length;
+
         view.$numberOfApprenticesLabel.text(apprenticeships.length);
         for(var i = 0; i < apprenticeships.length; i++) {
             var $tr = makeRow(apprenticeships[i]);
             view.$apprenticeshipsTableBody.append($tr);
         }
-
-        
     }
-
-
-
-
-
     function makeRow(apprenticeship) {
-<<<<<<< HEAD
 
-  
+        // changes heading, works when i put it in here
+        var newCopy = apprenticeship.training.name;
+        document.getElementById("apprenticeReplaceMe").innerHTML = numberOfApps + ' ' + newCopy;
 
         var displayCost = apprenticeship.cost ? '£' + apprenticeship.cost.format(0) : 'TBD';
         var displayStart = apprenticeship.start.month ? apprenticeship.start.month + '/' + apprenticeship.start.year : 'unknown';
         var displayFinish = apprenticeship.finish.month ? apprenticeship.finish.month + '/' + apprenticeship.finish.year : 'unknown';
 
-       
-
-=======
->>>>>>> parent of 982a1b0... Fix issue where if not all apprenticeship info is completed list does not display correctly
         var $tr = $('<tr></tr>');
-        $tr.append(makeCell(apprenticeship.training.name));
+       // $tr.append(makeCell(apprenticeship.training.name));
         $tr.append(makeCell(apprenticeship.apprentice.first));
         $tr.append(makeCell(apprenticeship.apprentice.last));
         $tr.append(makeCell(apprenticeship.apprentice.uln));
-        $tr.append(makeCell(apprenticeship.start.month + '/' + apprenticeship.start.year));
-        $tr.append(makeCell(apprenticeship.finish.month + '/' + apprenticeship.finish.year));
-        $tr.append(makeCell('£' + apprenticeship.cost.format(0)));
+        $tr.append(makeCell(displayStart));
+        $tr.append(makeCell(displayFinish));
+        $tr.append(makeCell(displayCost));
         $tr.append(makeActionCell(apprenticeship.id));
         return $tr;
-
     }
-
     function makeCell(text) {
-        return $('<td></td>').text(text);
+        return $('<td></td>').text(text || 'unknown');
     }
     function makeActionCell(id) {
         var source = commitmentListSource || 'employer';
