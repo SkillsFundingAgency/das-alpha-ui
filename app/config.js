116c390b09ad200fa947ee34e03e49e71225bd56
rob jenkins <rob@knowdigital.co.uk>
2017-01-25 14:26:08 +0000

    tweak

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Access my levy",
+  serviceName: "Manage apprentices",
 
   // Default port that prototype runs on
   port: '3000',
